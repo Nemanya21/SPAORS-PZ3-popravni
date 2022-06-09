daf3de4a33b8664bbd0551b50a9b339413830051
Nemanya21 <necavosa@gmail.com>
2022-06-09 15:59:33 +0200

    Obrisan komentar

--- TreciPopravni/src/Product.java
+++ TreciPopravni/src/Product.java
@@ -1,15 +1,9 @@
 
 public class Product {
 	
-	/*calcultes product of first n elements
-	 * 
-	 * n needs to be a positive number
-	 * 
-	 * */
-	
 	public int product(int n) {
 		int result = 1;
-		for(int i = 0; i < n; i++) {
+		for(int i = 1; i < n; i++) {
 			result = i;
 		}
 		return result;
