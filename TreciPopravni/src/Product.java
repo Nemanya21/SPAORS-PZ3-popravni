0f80a4274e61fffa6014272f9bb83970081007bf
Nemanya21 <necavosa@gmail.com>
2022-06-09 16:08:04 +0200

    Ispravljeni bugovi

--- TreciPopravni/src/Product.java
+++ TreciPopravni/src/Product.java
@@ -4,7 +4,11 @@ public class Product {
 	public int product(int n) {
 		int result = 1;
 		for(int i = 1; i < n; i++) {
+<<<<<<< HEAD
 			result = i;
+=======
+			result *= i;
+>>>>>>> Nemanja-Matic
 		}
 		return result;
 	}
